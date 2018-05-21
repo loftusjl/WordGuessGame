40c3b0e0f925319a37c8d4140696489cfe71f0a1
JesseL <35356853+loftusjl@users.noreply.github.com>
2018-05-21 10:51:48 -0700

    initial commented structure

---
+++ assets/javascript/game.js
@@ -0,0 +1,9 @@
+// get key
+    // press any start
+// wins
+// number of guesses
+// letters guessed array
+// word array
+// current word
+    // current word letters array
+    // current word placeholder
