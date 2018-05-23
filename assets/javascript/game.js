8b5fec3c907473aece573f85a087a884935474de
JesseL <35356853+loftusjl@users.noreply.github.com>
2018-05-23 16:03:49 -0700

    added press any key message on site. gave wrong guess logic influence over number of guesses remaining

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,8 +5,11 @@ let wins = 0;
 let hangWords = ['zombie', 'cthulu', 'chucky', 'dracula', 'jason', 'freddy', 'wolfman', 'damien', 'mephisto', 'pinhead', 'susan'];
 
 console.log(`Press any key to start`); // change to alert or HTML element
+// let anyKe/y = document.getElementById('anyKey');
+// $('#anyKey').show();
 
 document.onkeyup = function(anyKey) {
+  $('#anyKey').hide();
   // Determines which key was pressed. Will start the game.
   let guesses = 6;
 
@@ -18,7 +21,12 @@ document.onkeyup = function(anyKey) {
 
   // letters guessed array
   let lettersGuessed = [];
+    // for (var i = 0; i < currentWord.length; i++) {
+    //   lettersGuessed[i] = '_';
+    // }
 
+    let remain = currentWord.length;
+    console.log(remain);
   
     document.onkeyup = function(e) {
       let guess = e.key;
@@ -29,6 +37,7 @@ document.onkeyup = function(anyKey) {
       lettersGuessed.push(String(guess.toLowerCase()));
       
       let lettersFound = 0; // use lettersFound === currentWordArray.length as win condition
+      let guessesLeftElement = document.getElementById('guessesLeft');
       
       // check for correct guess. iterate either letters found or guesses.
       for(let i=0; i < currentWordArray.length; i++) { 
@@ -38,13 +47,27 @@ document.onkeyup = function(anyKey) {
       };
       
       if (lettersFound > 0) { // console feedback. change to alert or html at some point
+        
         console.log('You guessed wisely');
+      
       }
       else {
         console.log('You guessed poorly');
         guesses--;
+        guessesLeftElement.innerHTML = guesses;    
+
       }
       console.log('current guesses: '+ lettersGuessed.toLocaleString());
+      //adds guesses to HTML
+      $('#guessesMade').append(' ' + guess + ',');
+
+      if(guesses < 1) { //game over and reset. change from alert to high z-index html 
+        let playAgain = confirm("YOU LOSE! Play again?");
+        if (playAgain){
+          document.location.reload();
+        }
+        else {alert(`Game Over`)};
+    }
       
   };
 };
