7e3595477952c906e23d11dfddf612bd74fc1d3e
JesseL <JESSE.LOFTUS@GMAIL.COM>
2018-05-28 17:38:21 -0700

    built reset function to be called when win or loss

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,32 +1,40 @@
 let wins = 0;
 let losses = 0;
-
 let hangWords = ['zombie', 'cthulhu', 'chucky', 'dracula', 'jason', 'freddy', 'wolfman', 'damien', 'mephisto', 'pinhead', 'susan'];
+let currentWordDisplay = []; // build blank array to represent current word
+var currentWord = hangWords[Math.floor(Math.random() * hangWords.length)];
+var currentWordArray = currentWord.split('');
+var guesses = 6;
+var correctGuesses = 0;
+var lettersGuessed = [];
+
 
 console.log(`Press any key to start`); // change to alert or HTML element
 // let anyKey = document.getElementById('#anyKey');
-// $('#anyKey').show();
+$('#anyKey').show();
 
-document.onkeyup = function () {
-  $('#anyKey').hide();
+document.onkeyup = newGame();
+$('#anyKey').hide(); // change to alert
+$('#current-word').text(currentWordDisplay.join(' ')); // print blank current word to site
+
+function newGame() {
   // Determines which key was pressed. Will start the game.
-  let guesses = 6;
-  let correctGuesses = 0;
+  guesses = 6;
+  correctGuesses = 0;
+  lettersGuessed = [];
+  currentWord = hangWords[Math.floor(Math.random() * hangWords.length)];
+  currentWordArray = currentWord.split('');
+  currentWordDisplay = []; // build blank array to represent current word
   
-  // letters guessed array
-  let lettersGuessed = [];
-  // generate current word
-  let currentWord = hangWords[Math.floor(Math.random() * hangWords.length)];
-  console.log(currentWord);
-  // current word letters array
-  let currentWordArray = currentWord.split('');
+  $('#current-word').text(currentWordDisplay.join(' ')); // print blank current word to site
+  $('#guessesMade').text('');
+  console.log(currentWord); // check console to cheat... err developer stuff... yeah
   
-  let currentWordDisplay = []; // build blank array to represent current word
   for (i = 0; i < currentWordArray.length; i++) {
     currentWordDisplay.push('_');
   };
+};
   
-  $('#current-word').text(currentWordDisplay.join(' ')); // print blank current word to site
   
   let remain = currentWord.length;
   console.log(remain);
@@ -57,6 +65,7 @@ document.onkeyup = function () {
       wins++;
       $('#wins').text(wins);
       alert('You Win!');
+      newGame();
       
     }
     else if (lettersFound > 0) {}
@@ -70,8 +79,8 @@ document.onkeyup = function () {
         $('#losses').text(losses);
         
         alert("YOU LOSE!");
+        newGame();
       };
     };
     
   };
-};
