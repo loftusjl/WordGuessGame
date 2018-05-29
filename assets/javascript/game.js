9f2756300f1b3c21286d030838d8edbb52085b0c
JesseL <JESSE.LOFTUS@GMAIL.COM>
2018-05-29 13:50:53 -0700

    reworking win conditions. trying to reduce keypress function logic by breaking parts out into functions

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -17,8 +17,61 @@ document.onkeyup = newGame();
 $('#anyKey').hide(); // change to alert
 $('#current-word').text(currentWordDisplay.join(' ')); // print blank current word to site
 
+let remain = currentWord.length;
+console.log(remain);
+  
+document.onkeyup = function (e) {
+  let guess = e.key;
+  let lettersFound = 0; // use lettersFound === currentWordArray.length as win condition
+  
+  lettersGuessed.push(String(guess.toLowerCase())); // push guess into lettersGuessed[]
+  
+  // check for correct guess. iterate either letters found or guesses.
+  if (lettersGuessed.includes(guess)) {}
+  else{
+    for (let i = 0; i < currentWordArray.length; i++) {
+      if (currentWordArray[i].charAt(0) === guess) {
+        lettersFound++;
+        correctGuesses++;
+        currentWordDisplay[i] = guess;
+        $('#guessesMade').append(' ' + guess + ',');
+      }
+    };
+  };
+
+  $('#current-word').text(currentWordDisplay.join(' ')); // print current word with correct guesses to site
+
+  if (correctGuesses >= currentWordArray.length) {
+    youWin();
+  }
+  // else if (lettersFound > 0) {} // letter guessed but no win yet.
+  else {
+    guesses--;
+    $('#guessesLeft').text(guesses);
+    $('#guessesMade').append(' ' + guess + ',');
+    if (guesses < 1) { //game over and reset. change from alert to high z-index html 
+      youLose();
+    };
+  };
+};
+
+
+//------------------------FUNCTIONS------------------------//
+function youWin() {
+  wins++;
+  $('#wins').text(wins);
+  alert('You Win!');
+  newGame();
+};
+
+function youLose() {
+  losses++;
+  $('#losses').text(losses);
+  alert("YOU LOSE!");
+  newGame();
+};
+
 function newGame() {
-  // Determines which key was pressed. Will start the game.
   guesses = 6;
   correctGuesses = 0;
   lettersGuessed = [];
@@ -28,59 +81,10 @@ function newGame() {
   
   $('#current-word').text(currentWordDisplay.join(' ')); // print blank current word to site
   $('#guessesMade').text('');
+  $('#guessesLeft').text('6');
   console.log(currentWord); // check console to cheat... err developer stuff... yeah
   
   for (i = 0; i < currentWordArray.length; i++) {
     currentWordDisplay.push('_');
   };
 };
-  
-  
-  let remain = currentWord.length;
-  console.log(remain);
-  
-  document.onkeyup = function (e) {
-    let guess = e.key;
-    let lettersFound = 0; // use lettersFound === currentWordArray.length as win condition
-    let guessesLeftElement = document.getElementById('guessesLeft');
-
-    console.log(`You have: ${guesses} guesses remaining`);
-    console.log('You guessed: ' + guess);
-    
-    // push guess into lettersGuessed[]
-    lettersGuessed.push(String(guess.toLowerCase()));
-    
-    // check for correct guess. iterate either letters found or guesses.
-    for (let i = 0; i < currentWordArray.length; i++) {
-      if (currentWordArray[i].charAt(0) === guess) {
-        lettersFound++;
-        correctGuesses++;
-        currentWordDisplay[i] = guess;
-        $('#guessesMade').append(' ' + guess + ',');
-      }
-    };
-    // console.log(lettersFound);
-    $('#current-word').text(currentWordDisplay.join(' ')); // print blank current word to site
-    if (correctGuesses >= currentWordArray.length) {
-      wins++;
-      $('#wins').text(wins);
-      alert('You Win!');
-      newGame();
-      
-    }
-    else if (lettersFound > 0) {}
-    else {
-      guesses--;
-      guessesLeftElement.innerHTML = guesses;
-      $('#guessesMade').append(' ' + guess + ',');
-      
-      if (guesses < 1) { //game over and reset. change from alert to high z-index html 
-        losses++;
-        $('#losses').text(losses);
-        
-        alert("YOU LOSE!");
-        newGame();
-      };
-    };
-    
-  };
