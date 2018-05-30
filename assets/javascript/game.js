a4969080ebc44bfcd7113da30db0c7ccbe92d980
JesseL <35356853+loftusjl@users.noreply.github.com>
2018-05-29 20:39:02 -0700

    slight refactoring of good/bad guesses

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,51 +4,56 @@ let hangWords = ['zombie', 'cthulhu', 'chucky', 'dracula', 'jason', 'freddy', 'w
 let currentWordDisplay = []; // build blank array to represent current word
 var currentWord = hangWords[Math.floor(Math.random() * hangWords.length)];
 var currentWordArray = currentWord.split('');
+var str = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var alphaArray = str.split("");
 var guesses = 6;
 var correctGuesses = 0;
 var lettersGuessed = [];
 
 console.log(`Press any key to start`); // change to alert or HTML element
-// let anyKey = document.getElementById('#anyKey');
 $('#anyKey').show();
 
-document.onkeyup = newGame();
-$('#anyKey').hide(); // change to alert
-$('#current-word').text(currentWordDisplay.join(' ')); // print blank current word to site
+document.onkeyup = function () {
+  newGame();
+  $('#anyKey').hide(); // change to alert
 
-let remain = currentWord.length;
-console.log(remain);
-  
-document.onkeyup = function (e) {
-  let guess = e.key;
-  let lettersFound = 0; // use lettersFound === currentWordArray.length as win condition
-  // check for correct guess. iterate either letters found or guesses.
-  if (lettersGuessed.includes(guess)) { // ignore letters already guessed
-    console.log('choose another letter: ' + guess);
-  }
-  else{
-    lettersGuessed.push(String(guess.toLowerCase())); // push guess into lettersGuessed[]
-    $('#guessesMade').append(' ' + guess + ',');
+  let remain = currentWord.length;
+  console.log(remain);
 
-    for (let i = 0; i < currentWordArray.length; i++) {
-      if (currentWordArray[i].charAt(0) === guess) {
-        lettersFound++;
-        correctGuesses++;
-        currentWordDisplay[i] = guess;
-      }
-    };
-    $('#current-word').text(currentWordDisplay.join(' ')); // print current word with correct guesses to site
-  
-    if (correctGuesses >= currentWordArray.length) {
-      youWin();
-    }
-    // else if (lettersFound > 0) {} // letter guessed but no win yet.
+  document.onkeyup = function (e) {
+    let guess = e.key;
+    let lettersFound = 0; // use lettersFound === currentWordArray.length as win condition
+    // check for correct guess. iterate either letters found or guesses.
+    if (event.getModifierState("CapsLock")) {
+      alert('Turn off Caps Lock and try again.')
+    } // make sure CapsLock is off
     else {
-      guesses--;
-      $('#guessesLeft').text(guesses);
-      // $('#guessesMade').append(' ' + guess + ',');
-      if (guesses < 1) { //game over and reset. change from alert to high z-index html 
-        youLose();
+      if (alphaArray.includes(guess)) { // check if letter was typed
+        if (lettersGuessed.includes(guess.toLowerCase())) { // ignore letters already guessed
+          console.log('choose another letter: ' + guess);
+        } else {
+          lettersGuessed.push(String(guess)); // push guess into lettersGuessed[]
+          $('#guessesMade').append(' ' + guess + ',');
+
+          for (let i = 0; i < currentWordArray.length; i++) {
+            if (currentWordArray[i].charAt(0) === guess) {
+              lettersFound++;
+              correctGuesses++;
+              currentWordDisplay[i] = guess;
+            }
+          };
+          $('#current-word').text(currentWordDisplay.join(' ')); // print current word with correct guesses to site
+          
+          if (correctGuesses >= currentWordArray.length) {
+            youWin();
+          } else if (lettersFound > 0) {} else {
+            guesses--;
+            $('#guessesLeft').text(guesses);
+            if (guesses < 1) { //game over and reset. change from alert to high z-index html 
+              youLose();
+            };
+          };
+        };
       };
     };
   };
@@ -76,12 +81,12 @@ function newGame() {
   currentWord = hangWords[Math.floor(Math.random() * hangWords.length)];
   currentWordArray = currentWord.split('');
   currentWordDisplay = []; // build blank array to represent current word
-  
+
   $('#current-word').text(currentWordDisplay.join(' ')); // print blank current word to site
   $('#guessesMade').text('');
   $('#guessesLeft').text('6');
   console.log(currentWord); // check console to cheat... err developer stuff... yeah
-  
+
   for (i = 0; i < currentWordArray.length; i++) {
     currentWordDisplay.push('_');
   };
